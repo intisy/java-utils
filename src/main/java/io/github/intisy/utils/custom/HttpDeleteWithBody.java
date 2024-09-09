5866290a350932c956cf0505a8a59b3984d38519
Intisy <wilderpizza99@gmail.com>
2024-09-09 21:13:32 +0200

    Moved some customs over

---
+++ src/main/java/io/github/intisy/utils/custom/HttpDeleteWithBody.java
@@ -0,0 +1,28 @@
+package io.github.intisy.utils.custom;
+
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import java.net.URI;
+
+public class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
+
+    public static final String METHOD_NAME = "DELETE";
+
+    public HttpDeleteWithBody() {
+        super();
+    }
+
+    public HttpDeleteWithBody(final URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    public HttpDeleteWithBody(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD_NAME;
+    }
+}
---
+++ src/main/java/io/github/intisy/utils/custom/Wait.java
@@ -0,0 +1,35 @@
+package io.github.intisy.utils.custom;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Wait {
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private boolean variable = false;
+
+    public void waitForVariable(boolean variable) throws InterruptedException {
+        lock.lock();
+        try {
+            while (this.variable != variable) {
+                condition.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setVariable(boolean value) {
+        lock.lock();
+        try {
+            variable = value;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+    public boolean getVariable() {
+        return variable;
+    }
+}
