5318a2943d651b9e4c618c6f90d8e3c825474ac0
finn <wilderpizza99@gmail.com>
2024-11-06 19:28:56 +0100

    Added some ConnectionUtils

--- src/main/java/io/github/intisy/utils/utils/ConnectionUtils.java
+++ src/main/java/io/github/intisy/utils/utils/ConnectionUtils.java
@@ -1,15 +1,19 @@
 package io.github.intisy.utils.utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import io.github.intisy.simple.logger.StaticLogger;
+
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This class provides utility methods for handling HTTP connections.
  */
 public class ConnectionUtils {
-
     /**
      * Generates a curl command based on the provided HTTP connection, token, and payload.
      *
@@ -56,5 +60,105 @@ public class ConnectionUtils {
      */
     public static void printOutput(HttpURLConnection connection) throws IOException {
         System.out.println(getOutput(connection));
+    }
+
+    public static void connectionPayload(JsonObject jsonPayload, HttpURLConnection connection) throws IOException {
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonPayload.toString().getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+    }
+
+    public static JsonObject authorizedRequest(String apiUrl, String key) {
+        try {
+            HttpURLConnection connection = post(apiUrl);
+            connection.setRequestProperty("Authorization", key);
+            return handleResponse(connection);
+        } catch (IOException e) {
+            return handleException(e);
+        }
+    }
+
+    public static JsonObject handleException(Exception e) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("message", "An error occurred while handling the request: " + e.getMessage());
+        jsonObject.addProperty("type", "error");
+        return jsonObject;
+    }
+
+    public static JsonObject authorizedRequest(String apiUrl, String key, JsonObject jsonPayload) {
+        try {
+            HttpURLConnection connection = post(apiUrl);
+            connection.setRequestProperty("Authorization", key);
+            connectionPayload(jsonPayload, connection);
+            return handleResponse(connection);
+        } catch (IOException e) {
+            return handleException(e);
+        }
+    }
+
+    public static JsonObject request(String apiUrl, JsonObject jsonPayload) {
+        try {
+            HttpURLConnection connection = post(apiUrl);
+            connectionPayload(jsonPayload, connection);
+            return handleResponse(connection);
+        } catch (IOException e) {
+            return handleException(e);
+        }
+    }
+
+    public static JsonObject request(String apiUrl) throws IOException {
+        try {
+            HttpURLConnection connection = post(apiUrl);
+            return handleResponse(connection);
+        } catch (IOException e) {
+            return handleException(e);
+        }
+    }
+
+    public static HttpURLConnection post(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        return connection;
+    }
+
+    public static JsonObject handleResponse(HttpURLConnection connection) throws IOException {
+        int responseCode = connection.getResponseCode();
+        BufferedReader br;
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
+        String response = read(br);
+        JsonReader reader = new JsonReader(new StringReader(response));
+        reader.setLenient(true);
+        try {
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                StaticLogger.warning(jsonObject.toString());
+                if (jsonObject.get("message").getAsString().equals("Invalid key parameter")) {
+                    StaticLogger.error("Invalid API key found.");
+                    //TODO rework login
+                }
+            }
+            br.close();
+            return jsonObject;
+        } catch (Exception e) {
+            System.err.println("Failed to parse JSON: " + e.getMessage());
+            return new JsonObject();
+        }
+    }
+
+    public static String read(BufferedReader br) throws IOException {
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            response.append(line);
+        }
+        return response.toString();
     }
 }
