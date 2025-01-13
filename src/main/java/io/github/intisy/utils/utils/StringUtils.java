5460eef338df6b01b4ac66f0a79eaf9d01f68621
intisy <wilderpizza99@gmail.com>
2025-01-13 18:41:13 +0100

    Update StringUtils.java

--- src/main/java/io/github/intisy/utils/utils/StringUtils.java
+++ src/main/java/io/github/intisy/utils/utils/StringUtils.java
@@ -77,7 +77,7 @@ public class StringUtils {
             throw new RuntimeException(e);
         }
     }
-    
+
     public static List<String> splitString(String string) {
         List<String> result = new ArrayList<>();
         StringBuilder line = new StringBuilder();
@@ -95,5 +95,26 @@ public class StringUtils {
             }
         }
         return result;
+    }
+
+    public static String toTitleCase(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        String[] words = input.split(" ");
+        StringBuilder titleCase = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > 1) {
+                titleCase.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1).toLowerCase());
+            } else {
+                titleCase.append(word.toUpperCase());
+            }
+            titleCase.append(" ");
+        }
+
+        return titleCase.toString().trim();
     }
 }
